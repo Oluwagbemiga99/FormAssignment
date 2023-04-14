65bb6505a08003d12b6f7eef5414b81c660acb42
Oluwagbemiga99 <119671728+Oluwagbemiga99@users.noreply.github.com>
2023-04-14 18:23:53 +0100

    Added test implementation

---
+++ FormAssignmentTests/ShapeTests.cs
@@ -0,0 +1,55 @@
+﻿using FormAssignment;
+using System.Drawing;
+
+namespace FormAssignmentTests
+{
+    [TestClass]
+    public class ShapeTests
+    {
+        // This is used as a test implementation of the shape class
+        protected class TestShape : Shape
+        {
+            public override void CheckParam()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Draw()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // This tests the Param setter and gettter in the shape class 
+        [TestMethod]
+        public void Test_the_ParamSetterAndGetter()
+        {
+            // Arrange
+            var shape = new TestShape();
+            var expectedParam = new List<string> { "test" };
+
+            // Act
+            shape.Param = expectedParam;
+            var actualParam = shape.Param;
+
+            // Assert
+            CollectionAssert.AreEqual(expectedParam, actualParam);
+        }
+
+        // This tests the PaintCanvas setter and gettter in the shape class
+        [TestMethod]
+        public void Test_the_PaintCanvasSetterAndGetter()
+        {
+            // Arrange
+            var shape = new TestShape();
+            var expectedCanvas = new Canvas(new Bitmap(100, 100));
+
+            // Act
+            shape.PaintCanvas = expectedCanvas;
+            var actualCanvas = shape.PaintCanvas;
+
+            // Assert
+            Assert.AreEqual(expectedCanvas, actualCanvas);
+        }
+    }
+}
